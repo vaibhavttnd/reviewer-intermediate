f0f63929d229fef8b083aa40e78904be5ccc5a93
vaibhavttnd <vaibhav.gulati@tothenew.com>
2016-12-12 17:25:47 +0530

    Cookbooks for Reviewer Intermediate

--- cookbooks/b/recipes/default.rb
+++ cookbooks/b/recipes/default.rb
@@ -3,3 +3,22 @@
 # Recipe:: default
 #
 # Copyright (c) 2016 Vaibhav Gulati, All Rights Reserved.
+
+
+
+user_details = Chef::EncryptedDataBagItem.load("users", "user")
+
+user "#{user_details['uname']}" do
+  uid "#{user_details['uid']}"
+  gid "#{user_details['gid']}"
+  home "#{user_details['home']}"
+  shell "#{user_details['shell']}"
+  password "#{user_details['password']}"
+  supports manage_home: true
+end
+
+group "#{user_details['gname']}" do
+  members "#{user_details['uname']}"
+  append true
+  action :create
+end
